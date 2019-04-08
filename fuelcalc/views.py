51184d52347baded379b711c3c74d9b9d9fcb145
badekk <pawel.badysiak@gmail.com>
2019-04-08 21:19:54 +0200

    Added equations, basic functionality and Journey Cost result on site

--- fuelcalc/views.py
+++ fuelcalc/views.py
@@ -5,11 +5,23 @@ from .forms import CalculatorForm
 
 
 def calculator_forms(request):
-    form = CalculatorForm
-    context = {
-     'form': form
-    }
+    journey_cost = 0
+    if request.method == "POST":
+        form = CalculatorForm(request.POST)
+        if form.is_valid():
+            distance = form.cleaned_data['distance']
+            petrol_price = form.cleaned_data['petrol_price']
+            car_consumption = form.cleaned_data['car_consumption']
+            journey_cost = (distance * car_consumption) / 100 * petrol_price
+    else:
+        form = CalculatorForm()
+
+    context = {'form': form,
+               'journey_cost': journey_cost}
+
     return render(request, "fuelcalc/calculator.html", context)
+
+
 
 
 # def calculating(request):
